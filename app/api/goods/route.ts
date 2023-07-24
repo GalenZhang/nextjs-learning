513c71b18359dd3c7e2fed689caac925c5104d26
Galen Zhang <z60014840@163.com>
2023-07-24 22:43:23 +0800

    prisma db

--- app/api/goods/route.ts
+++ app/api/goods/route.ts
@@ -1,16 +1,35 @@
 
-import { NextResponse } from 'next/server'
+import { prisma } from '@/app/db'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET() {
+  const data = await prisma.goods.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
   return NextResponse.json({
     success: true,
-    errorMessage: '',
-    data: [{
-      id: 1,
-      name: '张三'
-    }, {
-      id: 2,
-      name: "李四"
-    }]
+    errorMessage: '获取数据成功',
+    data
+    // data: [{
+    //   id: 1,
+    //   name: '张三'
+    // }, {
+    //   id: 2,
+    //   name: "李四"
+    // }]
+  })
+}
+
+export const POST = async (req: NextRequest) => {
+  const data = await req.json()
+  await prisma.goods.create({
+    data
+  })
+  return NextResponse.json({
+    success: true,
+    errorMessage: '创建成功',
+    data
   })
 }
