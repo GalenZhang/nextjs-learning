1df7d266b82398f4ae88a8de2314436cadacb26e
Galen Zhang <z60014840@163.com>
2023-07-25 21:38:55 +0800

    use client

--- app/demo/list/page.tsx
+++ app/demo/list/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import React from 'react'
+import List from './_components/List'
 
 export const metadata: Metadata = {
   title: "这是一个列表页",
@@ -7,32 +8,41 @@ export const metadata: Metadata = {
   keywords: "nextjs,react"
 }
 
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/goods')
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+// async function getData() {
+//   const res = await fetch('http://localhost:3000/api/goods')
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
 
-  return res.json()
-}
+//   return res.json()
+// }
 
-type Item = {
-  id: number,
-  name: String
-}
+// type Item = {
+//   id: number,
+//   name: String
+// }
+
+// async function ListPage() {
+//   const data = await getData()
+//   console.log('getData for goods: ', data)
+//   return (
+//     <div>
+//       <ul>
+//         {
+//           data.data.map((item: Item) => (
+//             <li key={item.id}>{item.name}</li>
+//           ))
+//         }
+//       </ul>
+//     </div>
+//   )
+// }
 
 async function ListPage() {
-  const data = await getData()
-  console.log('getData for goods: ', data)
   return (
-    <div>
-      <ul>
-        {
-          data.data.map((item: Item) => (
-            <li key={item.id}>{item.name}</li>
-          ))
-        }
-      </ul>
+    <div className='list-page'>
+      ListPage
+      <List />
     </div>
   )
 }
